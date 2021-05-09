fd16868a29e4d52d6549d2d6f195a3d5763c4988
Carlos <shadysharly@outlook.cl>
2021-05-09 19:07:55 -0400

    Modificacion de Algoritmo a tipo recursivo

--- newtonRaphson.m
+++ newtonRaphson.m
@@ -1,22 +1,26 @@
 function root = newtonRaphson(polynomial, maxIter, tolerance, x0)
 
+if maxIter == 0
+    root = x0 - rem(x0, tolerance)
+    return;
+end
+
 fun = polynomial;             % Se asigna al polinomio de entrada como la funcion a aproximar
 funDiff = diff(fun);          % Se determina la derivada de primer orden de la funcion
 
-for i = 1: maxIter                       % Se itera hasta el maximo de iteraciones definido
-    fun_eval = polyval(fun, x0);         % Se evalua el valor aproximado actual en la funcion
-    funDiff_eval = polyval(funDiff, x0); % Se evalua el valor aproximado actual en la derivada
-    xi = x0 - (fun_eval / funDiff_eval); % Se determina la siguiente aproximacion de la sucesion
-    error = abs(xi - x0);                % Se determina el error
-    %fprintf('%f\n', xi);                  Se muestra la sucesion
-    if error < tolerance        % Si el error calculado es menor al umbral establecido, se deja
-        break                 % de iterar y se devuelve la nueva aproximacion
-    end
-    
-    x0 = xi;                  % En otro caso, se asigna la nueva aproximacion como la actual
+fun_eval = polyval(fun, x0);         % Se evalua el valor aproximado actual en la funcion
+funDiff_eval = polyval(funDiff, x0); % Se evalua el valor aproximado actual en la derivada
+xi = x0 - (fun_eval / funDiff_eval); % Se determina la siguiente aproximacion de la sucesion
+error = abs(xi - x0);                % Se determina el error
+%fprintf('%f\n', xi);                 % Se muestra la sucesion
+
+if error < tolerance        % Si el error calculado es menor al umbral establecido, se deja
+    root = xi - rem(xi, tolerance)
+    return;                 % de iterar y se devuelve la nueva aproximacion
 end
-
-root = xi - rem(xi, tolerance);    % Se devuelve la raiz con los decimales definidos
-
+    
+maxIter = maxIter - 1;
+x0 = xi;
+newtonRaphson(polynomial, maxIter, tolerance, x0);
 end
 
