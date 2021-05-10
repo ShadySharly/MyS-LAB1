4efca605036c5480b7bd78c9066476ca4c4a8e39
Carlos <shadysharly@outlook.cl>
2021-05-09 22:40:52 -0400

    Fix Archivo de Main

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ x2=-10:0.05:10;% Creacion de puntos para apartdado 3.1 parte 2
 %declaracion de a(x), b(x) y c(x) 
 a=(6*log2(5*x+15)/log2(4))-log2(2*x);
 b=sin(6*log10(2*x+9))+cos(2*log(x+16));
-c=2*exp(2*x2)+3;
+c=2*exp(2*x2+3);
 % Graficas 
 % a(x)
 figure(1)
@@ -15,7 +15,7 @@ ylabel('(6*log2(5*x+15)/log2(4))-log2(2*x)')
 %b(x)
 figure(2)
 plot(x,b,'g+')
-title("Grafica a(x)")
+title("Grafica b(x)")
 xlabel('x')
 ylabel('sin(6*log10(2*x+9))+cos(2*log(x+16))')
 %a(x) y b(x)
@@ -34,14 +34,14 @@ plot(x2,c,'g--')
 grid on 
 title("Grafica c(x) escala normal")
 xlabel('x')
-ylabel('2*exp(2*x2)+3')
+ylabel('2*exp(2*x2+3)')
 %c(x) escala logaritmica
 figure(5)
 semilogy (x2,c,'r--')
 grid on 
 title("Grafica c(x) escala logaritmica")
 xlabel('x')
-ylabel('2*exp(2*x2)+3')
+ylabel('2*exp(2*x2+3)')
 % Parte 2 
 %desviacion estandar
 desviacion=desviacionF([1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]);
